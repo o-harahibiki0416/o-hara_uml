65d5a1fce57c7390d807738bb33b8c96433811bc
o-hara_hibiki <kmm2459313@stu.o-hara.ac.jp>
2025-09-25 10:25:55 +0900

    a

---
+++ group/06_robo_dog/micro_site/controllers/greet_with_time.py
@@ -0,0 +1,7 @@
+from app_logic import set_new_time, get_my_greeting
+from utils import render_template
+
+def greet_with_time():
+    set_new_time()
+    get_my_greeting()
+    return render_template("boundaries/greet_with_time.html", greeting=get_my_greeting())
